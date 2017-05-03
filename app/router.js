66270d00674ae91ac861ddd150fa52aa60db3a0f
srihari.pr <srihari.pr@healthicity.com>
2017-05-03 17:14:50 +0530

    Ember-training: training files - contains basic project structure, routing and basic component operations

---
+++ app/router.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('bands',function(){
+    this.route('band',{path:':slug'}, function(){
+      this.route('songs');
+      this.route('albums');
+    });
+  });
+
+});
+
+export default Router;
---
+++ app/routes/bands.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+
+import Band from '../models/band';
+import Song from '../models/song';
+
+export default Ember.Route.extend({
+  model: function() {
+
+    var blackDog = Song.create({
+        title: 'Black Dog',
+        band: 'Led Zeppelin',
+        rating: 3
+      });
+      var yellowLedbetter = Song.create({
+        title: 'Yellow Ledbetter',
+        band: 'Pearl Jam',
+        rating: 4
+      });
+      var pretender = Song.create({
+        title: 'The Pretender',
+        band: 'Foo Fighters',
+        rating: 2
+      });
+      var daughter = Song.create({
+        title: 'Daughter',
+        band: 'Pearl Jam',
+        rating: 5
+      });
+    var ledZeppelin = Band.create({ name: 'Led Zeppelin', songs:[yellowLedbetter, daughter] });
+    var pearlJam = Band.create({ name: 'Pearl Jam', songs: [pretender] });
+    var fooFighters = Band.create({ name: 'Foo Fighters', songs:[blackDog] });
+    return [ledZeppelin, pearlJam, fooFighters];
+  },
+  actions: {
+    createBand: function() {
+      var name = this.get('controller').get('name');
+      var band = Band.create({ name: name });
+      this.modelFor('bands').pushObject(band);
+      this.get('controller').set('name', '');
+    }
+  }
+});
